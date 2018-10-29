8485e8fd689f00b5a51b60ae243b52168d385d5a
Sako <ssaakkooo@gmail.com>
2018-10-29 22:10:37 +0400

    initial commit

--- Stack_Sort.py
+++ Stack_Sort.py
@@ -18,3 +18,30 @@ class Stack:
         return len(self.stack)
     def is_empty(self):
         return self.size() == 0
+    def display(self):
+        print self.stack
+
+class StackSort:
+    def __init__(self):
+        self.input = Stack()
+        self.output = Stack()
+        self.tmp = Stack()
+
+    def sort(self, data):
+        for i in data:
+            self.input.push(i)
+        while self.input.is_empty() == False:
+            self.tmp = self.input.peak()
+            self.input.pop()
+            while self.output.is_empty() == False and int(self.output.peak()) < int(self.tmp):
+                self.input.push(self.output.peak())
+                self.output.pop()
+            self.output.push(self.tmp)
+        self.output.display()
+
+def main():
+
+    sortdata = StackSort()
+    sortdata.sort((7,2,9,4,0,5))
+
+main()
